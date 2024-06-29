c16747059be55eda663701f6752f2365dc89f09e
vchaga <durgaprasad3.ch@gmail.com>
2024-06-29 09:00:00 -0500

    Ver 0.3.1 trimUDF changes

--- src/main/scala/org/spark/learn/trimUDF.scala
+++ src/main/scala/org/spark/learn/trimUDF.scala
@@ -7,38 +7,38 @@ import org.apache.spark.sql.expressions.UserDefinedFunction
 
 object TrimUDF {
 
-	def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
 
-			val spark = SparkSession.builder()
-					.appName("withColumn")
-					.master("local[*]")
-					.getOrCreate()
+        val spark = SparkSession.builder()
+            .appName("withColumn")
+            .master("local[*]")
+            .getOrCreate()
+            
+        import spark.implicits._
+            
+        // Read CSV file into DataFrame
+        val dfCsv = spark.read.format("csv")
+            .option("header", true)
+            .csv("file:///c:/users/venka/Onedrive/desktop/BD/Data/df.csv")
+            
+        // Define and register the UDF
+        val trimStringUDF: UserDefinedFunction = udf((s: String) => {
+                if (s == null) null else s.trim
+            })
+            
+        spark.udf.register("trimString", trimStringUDF)
+            
+        // Apply the UDF to each string column in the dataset
+        val trimmedDf = dfCsv.columns.foldLeft(dfCsv) { (df, colName) =>
+        df.schema(colName).dataType match {
+        case StringType => df.withColumn(colName, trimStringUDF(col(colName)))
+        case _ => df
+        }
+        }
 
-					import spark.implicits._
+        // Show final schema and data
+        trimmedDf.printSchema()
+        trimmedDf.show()
 
-					// Read CSV file into DataFrame
-					val dfCsv = spark.read.format("csv")
-					.option("header", true)
-					.csv("file:///c:/users/venka/Onedrive/desktop/BD/Data/df.csv")
-
-					// Define and register the UDF
-					val trimStringUDF: UserDefinedFunction = udf((s: String) => {
-						if (s == null) null else s.trim
-					})
-
-					spark.udf.register("trimString", trimStringUDF)
-
-					// Apply the UDF to each string column in the dataset
-					val trimmedDf = dfCsv.columns.foldLeft(dfCsv) { (df, colName) =>
-					df.schema(colName).dataType match {
-					case StringType => df.withColumn(colName, trimStringUDF(col(colName)))
-					case _ => df
-					}
-			}
-
-			// Show final schema and data
-			trimmedDf.printSchema()
-			trimmedDf.show()
-
-	}
+    }
 }
